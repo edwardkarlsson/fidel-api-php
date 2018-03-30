f402998156fea78859d3b9b0d0e69fabf12b73de
Edward Karlsson <edward@medinaproduction.se>
2018-03-30 13:48:46 +0200

    Bugfix

--- src/Resources/TransactionsResource.php
+++ src/Resources/TransactionsResource.php
@@ -18,7 +18,7 @@ class TransactionsResource extends FidelResource
     {
         $resource = '/transactions/' . $transactionId;
 
-        return $this->fidelAPI->call($resource);
+        return $this->fidelAPI->call($resource)->processItems(Transaction::class);
     }
 
     /**
